a8d98beeaf096a5a9c0b260cf7ba798e3bcaa6bf
fengshaoying <fengshaoying@gmail.com>
2022-03-27 15:04:11 +0800

    Update n.js

--- docs/n.js
+++ docs/n.js
@@ -1,15 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <title>Nutstore</title>
-  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-  <meta name="description" content="Description">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0">
-</head>
-<body> 
 <script type="text/javascript">
 　　window.location.href="https://www.jianguoyun.com/p/De1ugIUQ4JD-CRi6yLQE";
 </script>
-</body>
-</html>
